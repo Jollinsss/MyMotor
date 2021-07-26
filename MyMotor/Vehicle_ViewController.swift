d4eb0cf337caebdc8b88b0792aa2266e36631145
Jack Collins <jcolli13@jaguarlandrover.com>
2021-07-26 21:52:08 +0100

    Adding add and dismiss buttons to Vehicle modal

--- MyMotor/Vehicle_ViewController.swift
+++ MyMotor/Vehicle_ViewController.swift
@@ -13,6 +13,9 @@ class Vehicle_ViewController: UIViewController
     @IBOutlet weak var vehicleModel: UILabel!
     @IBOutlet weak var vehicleRegistration: UILabel!
     
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var dismissbutton: UIButton!
+    
     
     override func viewDidLoad()
     {
@@ -23,4 +26,20 @@ class Vehicle_ViewController: UIViewController
         vehicleRegistration.text = DVSA.vehicleRegistration.uppercased()
     }
 
+    @IBAction func addVehicle()
+    {
+        let alert_success = UIAlertController(title: "Success", message: "Vehicle added to account.", preferredStyle: .alert)
+        let default_action = UIAlertAction(title: "Dismiss", style: .default, handler: {
+        _ in
+            self.dismiss(animated: true, completion: nil)
+        })
+        alert_success.addAction(default_action)
+        self.present(alert_success, animated: true, completion: nil)
+    }
+    
+    @IBAction func dismiss()
+    {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
